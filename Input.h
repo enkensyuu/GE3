e3b62f1c09324bef0a3bd1a18ddadf3474d18c24
DESKTOP-8KS70PI\siyum <k021g1321@g.neec.ac.jp>
2022-10-17 13:47:35 +0900

    02_01

---
+++ Input.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+
+class Input
+{
+public:
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize(HINSTANCE hInstance,HWND hwnd);
+
+	/// <summary>
+	/// XV
+	/// </summary>
+	void Update();
+};
+
