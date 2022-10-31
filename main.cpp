62728335bb089f53eb9c5c8388f29389c29cbda8
DESKTOP-8KS70PI\siyum <k021g1321@g.neec.ac.jp>
2022-10-31 16:01:39 +0900

    03_05改

--- main.cpp
+++ main.cpp
@@ -194,22 +194,6 @@ void UploadSubresources(ID3D12Resource* texBuff, const ScratchImage& scratchImg)
 	}
 }
 
-// ウィンドウプロシージャ
-LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	// メッセージ応じてゲーム固有の処理を行う
-	switch (msg) {
-		// ウィンドウが破棄された
-	case WM_DESTROY:
-		// OSに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	// 標準のメッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
-
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
