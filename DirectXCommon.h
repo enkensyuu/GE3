f213773599d9473077bd6271502a61eb7eb5d3e4
DESKTOP-8KS70PI\siyum <k021g1321@g.neec.ac.jp>
2022-11-07 14:18:36 +0900

    04_02

--- DirectXCommon.h
+++ DirectXCommon.h
@@ -1,10 +1,73 @@
 #pragma once
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+#include <vector>
+#include"WinApp.h"
+
 class DirectXCommon
 {
 public:
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	void Initialize(WinApp*winApp);
+
+private:
+	/// <summary>
+	/// デバイス初期化
+	/// </summary>
+	void InitializeDevice();
+
+	/// <summary>
+	/// コマンド関連の初期化
+	/// </summary>
+	void InitializeCommand();
+
+	/// <summary>
+	/// スワップチェーンの初期化
+	/// </summary>
+	void InitializeSwapchain();
+
+	/// <summary>
+	/// レンダーターゲットビューの初期化
+	/// </summary>
+	void InitializeRenderTargetView();
+
+	/// <summary>
+	/// 深度バッファーの初期化
+	/// </summary>
+	void InitializeDepthBuffer();
+
+	/// <summary>
+	/// フェンスの初期化
+	/// </summary>
+	void InitializeFence();
+private:
+	WinApp* winApp = nullptr;
+
+	// デバイス
+	Microsoft::WRL::ComPtr<ID3D12Device> device;
+	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
+
+	// コマンド関連
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
+
+	// スワップチェーン
+	Microsoft::WRL::ComPtr<IDXGISwapChain4> swapChain;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+
+	// レンダーターゲットビュー
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvHeap;
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> backBuffers;
+
+	// 深度バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> depthBuff;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> dsvHeap;
+
+	// フェンス
+	Microsoft::WRL::ComPtr<ID3D12Fence> fence;
 };
 
