863c8b26a815ea9b33af857b0fc247141495ce5f
DESKTOP-8KS70PI\siyum <k021g1321@g.neec.ac.jp>
2023-01-29 00:28:18 +0900

    04_02

--- SpriteCommon.h
+++ SpriteCommon.h
@@ -1,12 +1,21 @@
 #pragma once
 #include <d3d12.h>
 #include <wrl.h>
+#include <DirectXMath.h>
 
 #include "DirectXCommon.h"
 
 // スプライト共通部
 class SpriteCommon
 {
+public:
+	// 横方向ピクセル数
+	const size_t textureWidth = 256;
+	// 縦方向ピクセル数
+	const size_t textureHeight = 256;
+	// 配列の要素数
+	const size_t imageDataCount = textureWidth * textureHeight;
+
 public:	// メンバ関数
 
 	// 初期化
@@ -22,5 +31,12 @@ private:
 	Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState;
 
 	Microsoft::WRL::ComPtr<ID3D12RootSignature> roorSignature;
+
+	// 画像イメージデータ配列
+	DirectX::XMFLOAT4* imageData;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> texBuff;
+
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvHeap;
 };
 
